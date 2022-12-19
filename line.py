de1fa1a3ac06217f99a3ad12aabfae80a10c833e
Yasim <91527193+Yasimcd@users.noreply.github.com>
2022-12-19 15:52:17 -0800

    Update line.py

--- line.py
+++ line.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-tgt = pd.read_csv('/Users/yasimcruzdiaz/Desktop/SDCCD/Class 663 PYTHON 4/CODE/target.csv')
+tgt = pd.read_csv('target.csv')
 date_df = tgt['Volume']
 print(date_df.head())
